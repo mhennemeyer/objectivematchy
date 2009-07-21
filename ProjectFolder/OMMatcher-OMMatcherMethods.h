105906ab4348aa5531c64cb2b07971289154cc9c
Matthias Hennemeyer <mhennemeyer@noname.(none)>
2009-07-21 17:40:47 +0200

    Started working on documentation

--- ProjectFolder/OMMatcher-OMMatcherMethods.h
+++ ProjectFolder/OMMatcher-OMMatcherMethods.h
@@ -6,19 +6,38 @@
 //  Copyright 2009 Matthias Hennemeyer. All rights reserved.
 //  Released under the terms of the MIT Licence.
 
+/*!
+    @header OMMatcher-OMMatcherMethods
+    @abstract   Built in Matcher Methods.
+    @discussion Matcher Methods are instance methods of OMMatcher. All Built In Matchers are added as a category to OMMatcher.
+*/
 #import <Cocoa/Cocoa.h>
 #import "OMMatcher.h"
 
+/*!
+    @category    OMMatcher (OMMatcherMethods)
+    @abstract    Built in Matcher Methods.
+    @discussion  Matcher Methods are instance methods of OMMatcher. All Built In Matchers are added as a category to OMMatcher.
+*/
 @interface OMMatcher (OMMatcherMethods)
 
 /*!
     @method     eql:
     @abstract   Tests for equality.
-    @discussion Tests for equality by sending 'isEqualTo:' to the Object under test with the parameter applied to eql: as the parameter.
+    @discussion Tests for equality by sending 'isEqualTo:' to the Object Under Test with the parameter applied to eql: as the parameter.
+	@throws     OMFailure
     @param      anExpected The Object that The Object Under Test should be equal to.
     @result     anExpected 
 */
 - (id) eql:(id)anExpected;
+
+/*!
+    @method     match:
+    @abstract   Matches against aRegEx.
+    @discussion Object Under Test must be a String. Matches against the regular expression that is applied as a literal in the string argument.
+    @param      aRegEx A regular expression literal as a string.
+    @result     aRegEx
+*/
 - (id) match:(NSString *)aRegEx;
 - (id) containObject:(id)anExpected;
 - (id) respondToSelector:(SEL)selector;
